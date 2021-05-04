ec18dca28848611d61d466f8aaed6163603f676e
Raymond Lynch <daemitus@gmail.com>
2021-05-04 18:52:01 -0400

    More work and linting

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-message=$( jq -r '.commits[0].message' "${GITHUB_EVENT_PATH}" )
+message=$(jq -r '.commits[0].message' "${GITHUB_EVENT_PATH}")
 
 echo "> message=${message}"
 
@@ -14,7 +14,7 @@ if [ "${INPUT_ENABLED}" = true ]; then
 elif [ "${INPUT_ENABLED}" = false ]; then
   echo "> Action disabled: false"
   enabled=false
-elif echo "${message}" | grep -q "${INPUT_ENABLED}"; then
+elif echo "${message}" | grep -qF "${INPUT_ENABLED}"; then
   echo "> Action enabled: substring match"
   enabled=true
 else
@@ -50,13 +50,13 @@ echo "${INPUT_TOKEN}" | gh auth login --with-token
 
 # Setup artifact data
 artifact="${INPUT_ARTIFACT_PATH}"
-pluginName=$( jq -r '.Name' "${artifact}"/*.json )
-internalName=$( jq -r '.InternalName' "${artifact}"/*.json )
-assemblyVersion=$( jq -r '.AssemblyVersion' "${artifact}"/*.json )
+pluginName=$(jq -r '.Name' "${artifact}"/*.json)
+internalName=$(jq -r '.InternalName' "${artifact}"/*.json)
+assemblyVersion=$(jq -r '.AssemblyVersion' "${artifact}"/*.json)
 
 echo "> Configuring git user"
-authorName=$( jq -r '.commits[0].author.name' "${GITHUB_EVENT_PATH}" )
-authorEmail=$( jq -r '.commits[0].author.email' "${GITHUB_EVENT_PATH}" )
+authorName=$(jq -r '.commits[0].author.name' "${GITHUB_EVENT_PATH}")
+authorEmail=$(jq -r '.commits[0].author.email' "${GITHUB_EVENT_PATH}")
 git config --global user.name "${authorName}"
 git config --global user.email "${authorEmail}"
 
@@ -70,36 +70,40 @@ git fetch origin
 
 # Fixup the remote url so it can be pushed to
 echo "> Adding token to origin push url"
-originUrl=$( git config --get remote.origin.url | cut -d '/' -f 3- )
+originUrl=$(git config --get remote.origin.url | cut -d '/' -f 3-)
 originUrl="https://${INPUT_TOKEN}@${originUrl}"
 git config remote.origin.url "${originUrl}"
 
 # The branch name is hardcoded to the plugin's name.
 # If it already exists, hard reset to master.
 if git show-ref --quiet "refs/heads/${pluginName}"; then
-    echo "> Branch ${pluginName} already exists, reseting to master"
-    git checkout "${pluginName}"
-    git reset --hard master
+  echo "> Branch ${pluginName} already exists, reseting to master"
+  git checkout "${pluginName}"
+  git reset --hard master
 else
-    echo "> Creating new branch ${pluginName}"
-    git branch "${pluginName}"
-    git checkout "${pluginName}"
-    git push --set-upstream origin --force "${pluginName}"
+  echo "> Creating new branch ${pluginName}"
+  git branch "${pluginName}"
+  git checkout "${pluginName}"
+  git push --set-upstream origin --force "${pluginName}"
 fi
 
 # Copy the artifact where it needs to go
 cd ..
+
 if "${testing}"; then
-  echo "> Moving artifact to testing"
-  mv "${artifact}" "repo/testing/${internalName}"
-else
-  echo "> Moving artifact to plugins"
-  mv "${artifact}" "repo/plugins/${internalName}"
-  # If committing to prod, remove the testing plugin
   if [ -d "repo/testing/${internalName}" ]; then
     echo "> Deleting testing plugin directory"
     rm -rf "repo/testing/${internalName}"
   fi
+  echo "> Moving artifact to testing"
+  mv "${artifact}" "repo/testing/${internalName}"
+else
+  if [ -d "repo/plugins/${internalName}" ]; then
+    echo "> Deleting plugin directory"
+    rm -rf "repo/plugins/${internalName}"
+  fi
+  echo "> Moving artifact to plugins"
+  mv "${artifact}" "repo/plugins/${internalName}"
 fi
 cd repo
 
@@ -115,20 +119,20 @@ prRepo="${INPUT_PR_REPOSITORY}"
 
 # The PR title is the friendly name and assembly version
 prTitle="${pluginName} ${assemblyVersion}"
-if "${testing}"; then 
+if "${testing}"; then
   prTitle="[Testing] ${prTitle}"
 fi
 
 # The PR body is the body of the last commit
-prBody=$( echo "${message}" | tail -n +2)
+prBody=$(echo "${message}" | tail -n +2)
 
-prNumber=$( gh api repos/${prRepo}/pulls | jq ".[] | select(.head.ref == \"${pluginName}\") | .number" )
+prNumber=$(gh api repos/${prRepo}/pulls | jq ".[] | select(.head.ref == \"${pluginName}\") | .number")
 if [ "${prNumber}" ]; then
   echo "> Editing existing PR"
   gh api "repos/${prRepo}/pulls/${prNumber}" --silent --method PATCH -f "title=${prTitle}" -f "body=${prBody}" -f "state=open"
 else
   echo "> Creating PR"
-  gh pr create --repo "${prRepo}" --title "${prTitle}" --body "${prBody}"
+  #gh pr create --repo "${prRepo}" --title "${prTitle}" --body "${prBody}"
 fi
 
 echo "> Done"
